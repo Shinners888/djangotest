eb55ec124e95467c84cb82ec655e38e54a66251a
Shinners888 <sinead888888888@gmail.com>
2021-07-01 22:11:13 +0000

    fixed views

--- todo/views.py
+++ todo/views.py
@@ -3,7 +3,6 @@ from .models import Item
 from .forms import ItemForm
 
 
-# Create your views here.
 def get_todo_list(request):
     items = Item.objects.all()
     context = {
@@ -13,11 +12,11 @@ def get_todo_list(request):
 
 
 def add_item(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = ItemForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("get_todo_list")
+            return redirect('get_todo_list')
     form = ItemForm()
     context = {
         'form': form
@@ -27,11 +26,11 @@ def add_item(request):
 
 def edit_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
-    if request.method == "POST":
+    if request.method == 'POST':
         form = ItemForm(request.POST, instance=item)
         if form.is_valid():
             form.save()
-            return redirect("get_todo_list")
+            return redirect('get_todo_list')
     form = ItemForm(instance=item)
     context = {
         'form': form
@@ -43,10 +42,10 @@ def toggle_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
     item.done = not item.done
     item.save()
-    return redirect("get_todo_list")
+    return redirect('get_todo_list')
 
 
 def delete_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
     item.delete()
-    return redirect("get_todo_list")
+    return redirect('get_todo_list')
